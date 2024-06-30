d1aee89a46e05a21d35b1bc8d883266ba66fe162
nsedoff <nikolaysedovv@gmail.com>
2024-06-30 20:15:12 +0300

    ddd

--- coinglass.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://open-api-v3.coinglass.com/api/futures/openInterest/ohlc-history?exchange=Binance&symbol=BTCUSDT&interval=1d"
-
-headers = {"accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
